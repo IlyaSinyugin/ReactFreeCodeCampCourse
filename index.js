86dbfd511ae698792aa2aa25e220c09e8b3c362a
Ilya <ilya@sinyugin.ru>
2022-04-05 21:32:36 +0200

    creating component

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 // import React from "react";
 // import ReactDOM from "react-dom";
 
-const info = (
-    <div>
-        <img src="./react-logo.png" width="120"></img>
-        <h1>
-            Fun facts about React
-        </h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on Github</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
-)
 
-ReactDOM.render(info, document.getElementById('root'))
+
+function TestComponent() {
+    return(
+        <div>
+            <h1>
+                Reasong Why I wanna learn React
+            </h1>
+            <ul>
+                <li>It's awesome</li>
+                <li>Great foundation for Frontend</li>
+                <li>Vanilla JS can get overwhelming and frameworks make js easier</li>
+            </ul>
+        </div>
+    )
+}
+
+ReactDOM.render(<TestComponent />, document.getElementById('root'))
 
