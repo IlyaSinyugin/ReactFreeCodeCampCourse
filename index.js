2647885c525bf33bc591c1579c9f5fcfc95ecec9
Ilya <ilya@sinyugin.ru>
2022-04-06 15:07:01 +0200

    react test

--- index.js
+++ index.js
@@ -6,14 +6,29 @@
 function TestComponent() {
     return(
         <div>
+        <header>
+                <nav>
+                    <img src="react-logo.png" width="50px"></img>
+                </nav>
+            </header>
+
             <h1>
                 Reasong Why I wanna learn React
             </h1>
-            <ul>
+            <ol>
                 <li>It's awesome</li>
                 <li>Great foundation for Frontend</li>
                 <li>Vanilla JS can get overwhelming and frameworks make js easier</li>
-            </ul>
+            </ol>
+            <footer>
+            <small>© 2022 Sinyugin development. All rights reserved.</small>
+            </footer>
+            <h2>
+                Testing the footer and h2 together
+            </h2>
+            <footer>
+                another footer
+            </footer>
         </div>
     )
 }
