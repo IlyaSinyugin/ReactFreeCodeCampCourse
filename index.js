ca616ad63ebb05a61715cc52bc3b9df4c388eca0
Ilya <ilya@sinyugin.ru>
2022-04-10 22:03:41 +0200

    custom components

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
 // import React from "react";
 // import ReactDOM from "react-dom";
 
+function Header() {
+    return(
+        <header>
+            <nav>
+                <img src="react-logo.png" width="50px"></img>
+            </nav>
+        </header>
+    )
 
+}
 
-function TestComponent() {
+function Footer() {
+    return(
+        <footer>
+            <small>© 2022 Sinyugin development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function MainContent() {
     return(
         <div>
-        <header>
-                <nav>
-                    <img src="react-logo.png" width="50px"></img>
-                </nav>
-            </header>
-
             <h1>
-                Reasong Why I wanna learn React
+                Reason Why I wanna learn React
             </h1>
             <ol>
                 <li>It's awesome</li>
                 <li>Great foundation for Frontend</li>
                 <li>Vanilla JS can get overwhelming and frameworks make js easier</li>
             </ol>
-            <footer>
-            <small>© 2022 Sinyugin development. All rights reserved.</small>
-            </footer>
-            <h2>
-                Testing the footer and h2 together
-            </h2>
-            <footer>
-                another footer
-            </footer>
         </div>
     )
 }
 
-ReactDOM.render(<TestComponent />, document.getElementById('root'))
+function Page() {
+    return(
+        <div>
+        <Header />
+        <MainContent />
+        <Footer />
+        </div>
+    )
+}
+
+ReactDOM.render(<Page />, document.getElementById('root'))
 
